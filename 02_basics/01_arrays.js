71ccc1f458d2d5085342a2adaff146fc4f212a2f
AshwaniRaii <ashwanirai975@gmail.com>
2024-10-02 21:54:59 +0530

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,5 +4,10 @@ const myArr = [0,1,2,3,4,5];
 const myHeros = ['Bose','Singh'];
 
 const newArr = new Array('Ashwani','Shivam')
-console.log(myArr[1])
-console.log(myHeros[1])
+// console.log(myArr[1])
+// console.log(myHeros[1])
+
+//Array Methods 
+myArr.push(6); //add value is last in array
+myArr.pop() // remove last value in array
+console.log(myArr);
