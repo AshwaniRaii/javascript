2af3620a7f28dc9a79527a33161ad3040e089807
AshwaniRaii <ashwanirai975@gmail.com>
2024-10-03 22:04:47 +0530

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,7 +3,7 @@
 const myArr = [0, 1, 2, 3, 4, 5];
 const myHeros = ["Bose", "Singh"];
 
-const newArr = new Array("Ashwani", "Shivam");
+const newArrs = new Array("Ashwani", "Shivam");
 // console.log(myArr[1])
 // console.log(myHeros[1])
 
@@ -17,4 +17,23 @@ const newArr = new Array("Ashwani", "Shivam");
 //the unshift can add a value in starting of array
 // myArr.unshift('john')
 // ['john', 0,1,2,3,4,5]
-console.log(myArr)
+// they remove starting value of array they dont need any parameter 
+// myArr.shift();
+[ 0, 1, 2, 3, 4, 5 ]
+
+//they behave like a question they check Singh are found this heros array or not they return boolean
+// console.log(myHeros.includes('Singh'));
+// true
+
+// console.log(myHeros.indexOf('hitesh'));
+// -1  means they not found value and index in this array
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// [ 0, 1, 2, 3, 4, 5 ]
+// console.log(typeof newArr)
+// 0,1,2,3,4,5  //they bind the value and change array to string
+
+
+
+
