af9533482daffeb6f227bd90f336f6110c49cd6e
AshwaniRaii <ashwanirai975@gmail.com>
2024-10-03 21:51:12 +0530

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,13 +1,20 @@
 //array
 
-const myArr = [0,1,2,3,4,5];
-const myHeros = ['Bose','Singh'];
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["Bose", "Singh"];
 
-const newArr = new Array('Ashwani','Shivam')
+const newArr = new Array("Ashwani", "Shivam");
 // console.log(myArr[1])
 // console.log(myHeros[1])
 
-//Array Methods 
-myArr.push(6); //add value is last in array
-myArr.pop() // remove last value in array
-console.log(myArr);
+//Array Methods
+
+//add value is last in array
+// myArr.push(6);
+// remove last value in array
+// myArr.pop();
+
+//the unshift can add a value in starting of array
+// myArr.unshift('john')
+// ['john', 0,1,2,3,4,5]
+console.log(myArr)
